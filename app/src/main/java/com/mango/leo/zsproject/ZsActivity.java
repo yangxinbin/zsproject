0ec2649374e8af6d0f736a7a9e2d45ccd1fd0e1f
yangxinbin <yangxinbin@hotmail.com>
2018-05-10 21:50:28 +0800

    搭建底部导航栏框架

---
+++ app/src/main/java/com/mango/leo/zsproject/ZsActivity.java
@@ -0,0 +1,44 @@
+package com.mango.leo.zsproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.mango.leo.zsproject.datacenter.show.FragmentOfDateCenter;
+import com.mango.leo.zsproject.eventexhibition.show.FragmentOfEventExhibition;
+import com.mango.leo.zsproject.industrialpanorama.show.FragmentOfIndustrialPanorama;
+import com.mango.leo.zsproject.industrialservice.show.FragmentOfIndustrialService;
+import com.mango.leo.zsproject.personalcenter.show.FragmentOfPersonalCenter;
+import com.mango.leo.zsproject.viewutil.BottomBar;
+
+public class ZsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BottomBar bottomBar = (BottomBar) findViewById(R.id.bottom_bar);
+        bottomBar.setContainer(R.id.fl_container)
+                .setTitleBeforeAndAfterColor("#999999", "#ff5d5e")
+                .addItem(FragmentOfIndustrialService.class,
+                        "产业服务",
+                        R.drawable.tab_icon_first_normal,
+                        R.drawable.tab_icon_first_highlight)
+                .addItem(FragmentOfIndustrialPanorama.class,
+                        "产业全景",
+                        R.drawable.tab_icon_second_normal,
+                        R.drawable.tab_icon_second_highlight)
+                .addItem(FragmentOfDateCenter.class,
+                        "数据中心",
+                        R.drawable.tab_icon_third_normal,
+                        R.drawable.tab_icon_third_highlight)
+                .addItem(FragmentOfEventExhibition.class,
+                        "活动展会",
+                        R.drawable.tab_icon_fourth_normal,
+                        R.drawable.tab_icon_fourth_highlight)
+                .addItem(FragmentOfPersonalCenter.class,
+                        "我的",
+                        R.drawable.tab_icon_fifth_normal,
+                        R.drawable.tab_icon_fifth_highlight)
+                .build();
+    }
+}
---
+++ app/src/main/java/com/mango/leo/zsproject/industrialpanorama/show/FragmentOfIndustrialPanorama.java
@@ -0,0 +1,19 @@
+package com.mango.leo.zsproject.industrialpanorama.show;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.mango.leo.zsproject.R;
+
+
+public class FragmentOfIndustrialPanorama extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return LayoutInflater.from(getActivity()).inflate(R.layout.industrialpanorama, container, false);
+    }
+}
