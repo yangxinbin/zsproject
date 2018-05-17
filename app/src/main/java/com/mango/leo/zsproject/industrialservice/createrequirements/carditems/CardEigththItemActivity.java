ba6ce9854412bd054443865dab20f585ae13a31b
admin <yangxinbin@hotmail.com>
2018-05-17 14:37:16 +0800

    项目描述页面提交

---
+++ app/src/main/java/com/mango/leo/zsproject/industrialservice/createrequirements/carditems/CardEigththItemActivity.java
@@ -0,0 +1,15 @@
+package com.mango.leo.zsproject.industrialservice.createrequirements.carditems;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.mango.leo.zsproject.R;
+
+public class CardEigththItemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_card_eigthth_item);
+    }
+}
---
+++ app/src/main/java/com/mango/leo/zsproject/industrialservice/createrequirements/carditems/CardSixthItemActivity.java
@@ -0,0 +1,15 @@
+package com.mango.leo.zsproject.industrialservice.createrequirements.carditems;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.mango.leo.zsproject.R;
+
+public class CardSixthItemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_card_sixth_item);
+    }
+}
