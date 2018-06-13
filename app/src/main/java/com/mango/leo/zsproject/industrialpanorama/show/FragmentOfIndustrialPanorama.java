5cbf36e95b92283b14c948dd06d67c6b40b0f0b7
admin <yangxinbin@hotmail.com>
2018-06-13 13:21:18 +0800

    分享修改

--- app/src/main/java/com/mango/leo/zsproject/industrialpanorama/show/FragmentOfIndustrialPanorama.java
+++ app/src/main/java/com/mango/leo/zsproject/industrialpanorama/show/FragmentOfIndustrialPanorama.java
@@ -16,7 +16,6 @@ import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.lljjcoder.Interface.OnCityItemClickListener;
@@ -39,6 +38,7 @@ import java.util.List;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import cn.sharesdk.onekeyshare.OnekeyShare;
 
 
 public class FragmentOfIndustrialPanorama extends Fragment implements AdapterView.OnItemClickListener {
@@ -97,10 +97,6 @@ public class FragmentOfIndustrialPanorama extends Fragment implements AdapterVie
         ButterKnife.unbind(this);
     }
 
-    @OnClick(R.id.city)
-    public void onViewClicked() {
-        showSeleteCity();
-    }
 
     private void showPopupWindow(Context context, List<String> listDate) {
         //设置要显示的view
@@ -152,16 +148,49 @@ public class FragmentOfIndustrialPanorama extends Fragment implements AdapterVie
                 if (district != null) {
                     districtString = String.valueOf(district);
                 }
-                city_t.setText(province + "" + city + district);
+                city_t.setText("" + city + district);
             }
 
             @Override
             public void onCancel() {
-                 AppUtils.showToast(getContext(), "城市选择已取消");
+                AppUtils.showToast(getContext(), "城市选择已取消");
             }
         });
 
         //显示
         mPicker.showCityPicker();
+    }
+
+    private void newShare() {
+        OnekeyShare oks = new OnekeyShare();
+        //关闭sso授权
+        oks.disableSSOWhenAuthorize();
+
+        // title标题，微信、QQ和QQ空间等平台使用
+        //oks.setTitle(title);
+        // titleUrl QQ和QQ空间跳转链接
+        //oks.setTitleUrl(newsurl);
+        // text是分享文本，所有平台都需要这个字段
+        oks.setText("活动详情");
+        // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
+        oks.setImageUrl("http:\\/\\/04.imgmini.eastday.com\\/mobile\\/20180610\\/20180610002454_6ab3e88e0c01455e287a65eda6bf7b1a_8_mwpm_03200403.jpg");//确保SDcard下面存在此张图片
+        // url在微信、微博，Facebook等平台中使用
+        oks.setUrl("http://192.168.1.166:8080/jetc/#/iosactivityDetail/:");
+        // comment是我对这条分享的评论，仅在人人网使用
+        oks.setComment("评论");
+        // 启动分享GUI
+        oks.show(getActivity());
+    }
+
+    @OnClick({R.id.image_msg2, R.id.city})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.image_msg2:
+                newShare();
+                break;
+            case R.id.city:
+                showSeleteCity();
+                break;
+        }
     }
 }
