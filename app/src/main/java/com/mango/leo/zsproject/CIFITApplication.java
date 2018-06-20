894d2c2af52c59ba22b4c858060c35a6a721df3d
admin <yangxinbin@hotmail.com>
2018-06-20 09:40:44 +0800

    mob初始化

--- app/src/main/java/com/mango/leo/zsproject/CIFITApplication.java
+++ app/src/main/java/com/mango/leo/zsproject/CIFITApplication.java
@@ -17,6 +17,6 @@ public class CIFITApplication extends Application {
         //自4.3.0起，百度地图SDK所有接口均支持百度坐标和国测局坐标，用此方法设置您使用的坐标类型.
         //包括BD09LL和GCJ02两种坐标，默认是BD09LL坐标。
         SDKInitializer.setCoordType(CoordType.BD09LL);
-        MobSDK.init(this);
+        MobSDK.init(getApplicationContext(), "264de1c3bdfe9", "3df56a487772e2af23564c4d553bd4ae");
     }
 }
