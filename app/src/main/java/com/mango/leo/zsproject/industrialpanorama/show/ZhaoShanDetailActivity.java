bb707c1f9512a7dbc984514242d8e90e4bdc181d
admin <yangxinbin@hotmail.com>
2018-06-28 12:28:22 +0800

    UI更新加载框

--- app/src/main/java/com/mango/leo/zsproject/industrialpanorama/show/ZhaoShanDetailActivity.java
+++ app/src/main/java/com/mango/leo/zsproject/industrialpanorama/show/ZhaoShanDetailActivity.java
@@ -4,10 +4,13 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.mango.leo.zsproject.R;
 import com.mango.leo.zsproject.utils.Urls;
@@ -25,6 +28,10 @@ public class ZhaoShanDetailActivity extends AppCompatActivity {
     ImageView imageViewZhaoshanShare;
     @Bind(R.id.web_zhaoshan)
     WebView webZhaoshan;
+    @Bind(R.id.loading)
+    ProgressBar loading;
+    @Bind(R.id.tv_load)
+    TextView tvLoad;
     private String id;
 
     @Override
@@ -37,22 +44,13 @@ public class ZhaoShanDetailActivity extends AppCompatActivity {
         WebSettings webSettings = webZhaoshan.getSettings();
         webSettings.setJavaScriptEnabled(true);
         initW(getIntent().getStringExtra("FavouriteId"));
-        //webview.loadUrl("http://www.baidu.com");
-        /*webZhaoshan.setWebViewClient(new WebViewClient(){
-            @Override
-            public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                // TODO Auto-generated method stub
-                //返回值是true的时候控制去WebView打开，为false调用系统浏览器或第三方浏览器
-                view.loadUrl(url);
-                return true;
-            }
-        });*/
-       //?? webZhaoshan.loadUrl("http://192.168.1.166:8080/jetc/#/iosactivityDetail/:"+id);
     }
+
     private void initW(final String sp) {
         webZhaoshan.setVisibility(View.VISIBLE);
         WebSettings webSettings = webZhaoshan.getSettings();
         webSettings.setJavaScriptEnabled(true);
+        webZhaoshan.loadUrl("http://47.106.184.121/jetc/#/iosdeclareDetailnobtn/:" + sp);
         webZhaoshan.setWebViewClient(new WebViewClient() {
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
@@ -62,8 +60,22 @@ public class ZhaoShanDetailActivity extends AppCompatActivity {
                 return true;
             }
         });
-        webZhaoshan.loadUrl("http://47.106.184.121/jetc/#/iosdeclareDetailnobtn/:" + sp);
+        webZhaoshan.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                if (newProgress == 100) {
+                    loading.setVisibility(View.GONE);
+                    tvLoad.setVisibility(View.GONE);
+                } else {
+                    loading.setVisibility(View.VISIBLE);
+                    tvLoad.setVisibility(View.VISIBLE);
+                    loading.setProgress(newProgress);
+                }
+                super.onProgressChanged(view, newProgress);
+            }
+        });
     }
+
     @OnClick({R.id.imageView_zhaoshan_back, R.id.imageView_zhaoshan_share})
     public void onViewClicked(View view) {
         switch (view.getId()) {
@@ -75,6 +87,7 @@ public class ZhaoShanDetailActivity extends AppCompatActivity {
                 break;
         }
     }
+
     private void newShare() {
         OnekeyShare oks = new OnekeyShare();
         //关闭sso授权
@@ -87,10 +100,10 @@ public class ZhaoShanDetailActivity extends AppCompatActivity {
         // text是分享文本，所有平台都需要这个字段
         oks.setText("招商信息");
         // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
-        oks.setImageUrl(Urls.HOST+"/user-service/user/get/file?fileId=5b1f70641233c531ec362024");//确保SDcard下面存在此张图片
+        oks.setImageUrl(Urls.HOST + "/user-service/user/get/file?fileId=5b1f70641233c531ec362024");//确保SDcard下面存在此张图片
         // url在微信、微博，Facebook等平台中使用
-        Log.v("sssssssssss","===="+"http://47.106.184.121/jetc/#/iosdeclareDetailnobtn/:" +getIntent().getStringExtra("FavouriteId"));
-        oks.setUrl("http://47.106.184.121/jetc/#/iosdeclareDetailnobtn/:" +getIntent().getStringExtra("FavouriteId"));
+        Log.v("sssssssssss", "====" + "http://47.106.184.121/jetc/#/iosdeclareDetailnobtn/:" + getIntent().getStringExtra("FavouriteId"));
+        oks.setUrl("http://47.106.184.121/jetc/#/iosdeclareDetailnobtn/:" + getIntent().getStringExtra("FavouriteId"));
         // comment是我对这条分享的评论，仅在人人网使用
         oks.setComment("评论");
         // 启动分享GUI
