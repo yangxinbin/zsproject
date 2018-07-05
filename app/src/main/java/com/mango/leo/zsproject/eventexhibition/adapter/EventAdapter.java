3825aece24c10c37a32d53eb0df35ab2e01610ce
admin <yangxinbin@hotmail.com>
2018-07-05 11:04:59 +0800

    活动标签更新

--- app/src/main/java/com/mango/leo/zsproject/eventexhibition/adapter/EventAdapter.java
+++ app/src/main/java/com/mango/leo/zsproject/eventexhibition/adapter/EventAdapter.java
@@ -133,7 +133,7 @@ public class EventAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> 
             if (((ItemViewHolder) holder) != null && mData.get(pos).getResponseObject() != null) {
                 Log.v("yyyyy", "====pos======" + pos % 20);//
                 if (mData.get(pos).getResponseObject().getContent() != null && mData.get(pos).getResponseObject().getContent().get(pos % 20) != null) {
-                    ((ItemViewHolder) holder).e_title.setText(mData.get(pos).getResponseObject().getContent().get(pos % 20).getName());
+                    ((ItemViewHolder) holder).e_title.setText("         "+mData.get(pos).getResponseObject().getContent().get(pos % 20).getName());
                     ((ItemViewHolder) holder).e_place.setText(mData.get(pos).getResponseObject().getContent().get(pos % 20).getLocation().getCity().toString());
                     ((ItemViewHolder) holder).e_time.setText(DateUtil.getDateToString(mData.get(pos).getResponseObject().getContent().get(pos % 20).getStartTime(), "yyyy-MM-dd"));
                     if (mData.get(pos).getResponseObject().getContent().get(pos % 20).getBanner() != null){
